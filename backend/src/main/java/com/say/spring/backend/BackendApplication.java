361c50534bdaf70e120d28313214bb754f2207f9
ysa <ysa.developer@gmail.com>
2021-05-21 22:26:04 +0700

    add new Maven framework

---
+++ backend/src/main/java/com/say/spring/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.say.spring.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ysa
+ */
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class,args);
+    }
+}
